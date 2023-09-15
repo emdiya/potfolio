a08c73a8d32409accb895c0eaf412a1143f440d7
diya <infor.emdiya@gmail.com>
2023-09-15 19:43:59 +0700

    Update Infor

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,6 +1,7 @@
 import "./Home.css";
-
-import Me from "../../assets/my-profile.png";
+import CircularImage from './CicularImg';
+// import Me from "../../assets/my-profile.png";
+// import Me from "../../assets/my_photo.jpeg"
 import HomeSocials from "./HeaderSocials";
 import ScrollDown from "./ScrollDown";
 
@@ -8,7 +9,8 @@ const Home = () => {
   return (
     <section className="home container" id="home">
       <div className="intro">
-        <img src={Me} alt="" className="home__image" />
+        {/* <img src={Me} alt="" className="home__image" /> */}
+        <CircularImage />
         <h1 className="home__name">EM  DiYA</h1>
         <span className="home__education">I'm a Mid-Senior Flutter Developer</span>
         <HomeSocials />
